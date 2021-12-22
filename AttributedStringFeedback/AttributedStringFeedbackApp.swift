71d29c6946002bb1172fe3725c524fd4e9322713
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-12-22 13:12:20 -0500

    Initial Commit

---
+++ AttributedStringFeedback/AttributedStringFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttributedStringFeedbackApp.swift
+//  AttributedStringFeedback
+//
+//  Created by Ryan Lintott on 2021-12-22.
+//
+
+import SwiftUI
+
+@main
+struct AttributedStringFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
